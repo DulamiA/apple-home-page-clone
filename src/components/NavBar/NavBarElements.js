621485f7701ea2f2c4a4c3e7776a911a520d9672
DulamiA <dulami.kaludewa@gmail.com>
2024-01-21 12:56:21 +0530

    navbar

---
+++ src/components/NavBar/NavBarElements.js
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import { NavLink as Link } from 'react-router-dom'
+import {FaBars} from 'react-icons/fa'
+
+export const Nav = styled.nav`
+    position: fixed;
+    top: 0;
+    z-index:1400;
+    width: 100%;
+    height: 4.4rem;
+    background-color: rgba(0,0,0,.8);
+    backdrop-filter: blur(2rem);
+    display: flex;
+    align-items: center;
+`
+
+export const Container = styled.div`
+    max-width:98rem;
+    margin: 0 auto;
+    padding: 0 2.2rem;
+    display: flex;
+    justify-content: space-between;
+`
+
+export const NavMenue = styled.div`
+    display: flex;
+    align-items: center;
+
+    @media screen and (max-width: 780px){
+        display: none;
+    }
+`
+
+export const NavLogoLink = styled(Link)`
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    cursor: pointer;
+    opacity: .8;
+
+    &:hover {
+        opacity: .98;
+        transition: 300ms ease-in;
+    }
+`
+
+export const NavLink = styled(Link)`
+    color: #fff;
+    display: flex;
+    font-size: 1.2rem;
+    align-items: center;
+    text-decoration: none;
+    padding-left: 2rem;
+    padding-right: 1.5rem;
+    height: 100%;
+    cursor: pointer;
+    opacity: .8;
+
+    &:hover {
+        opacity: .98;
+        transition: 300ms ease-in;
+    }
+`
+
+export const Bars = styled(FaBars)`
+    display: none;
+    color: #fff;
+
+    @media screen and (max-width: 760px){
+        display: block;
+        position: absolute;
+        top: 0;
+        right: 0;
+        transform: translate(-100%, 75%);
+        font-size: 1.8rem;
+        cursor: pointer;
+    }
+`
+
+
+
---
+++ src/components/NavBar/NavIndex.js
@@ -0,0 +1,75 @@
+import React from "react";
+import AppleLogo from '../../images/icons/apple.svg';
+import SearchLogo from '../../images/icons/search.svg';
+import BagLogo from '../../images/icons/bag.svg';
+import { Nav, NavLink, Bars, NavMenue, NavLogoLink, Container} from './NavBarElements'
+
+function NavBar(){
+    return (
+        <>
+            <Nav>
+                <Container>
+                    <NavLogoLink to = '/'>
+                        <img 
+                            src={AppleLogo}
+                            className="nav-logo"
+                            alt="appleLogo"
+                        />
+                    </NavLogoLink>
+                    <Bars/>
+                    <NavMenue>
+                        <NavLink to = '/store' activeStyle>
+                            Store
+                        </NavLink>
+                        <NavLink to = '/mac' activeStyle>
+                            Mac
+                        </NavLink>
+                        <NavLink to = '/ipad' activeStyle>
+                            iPad
+                        </NavLink>
+                        <NavLink to = '/iphone' activeStyle>
+                            iPhone
+                        </NavLink>
+                        <NavLink to = '/watch' activeStyle>
+                            Watch
+                        </NavLink>
+                        <NavLink to = '/vision' activeStyle>
+                            Vision
+                        </NavLink>
+                        <NavLink to = '/airpods' activeStyle>
+                            AirPods
+                        </NavLink>
+                        <NavLink to = '/tv&home' activeStyle>
+                            TV & Home
+                        </NavLink>
+                        <NavLink to = '/entertainment' activeStyle>
+                            Entertainment
+                        </NavLink>
+                        <NavLink to = '/accessories' activeStyle>
+                            Accessories
+                        </NavLink>
+                        <NavLink to = '/support' activeStyle>
+                            Support
+                        </NavLink>
+                    </NavMenue>
+                    <NavLogoLink to = '/search'>
+                        <img
+                            src={SearchLogo}
+                            className="nav-logo"
+                            alt="searchLogo"
+                        />
+                    </NavLogoLink>
+                    <NavLogoLink to = '/bag'>
+                        <img
+                            src={BagLogo}
+                            className="nav-logo"
+                            alt="bagLogo"
+                        />
+                    </NavLogoLink>
+                </Container>
+            </Nav>
+        </>
+    )
+}
+
+export default NavBar
