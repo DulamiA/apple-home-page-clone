98650e854322ce930cc6e3f99fc2cd68093bec0c
DulamiA <dulami.kaludewa@gmail.com>
2024-01-23 13:11:38 +0530

    navBar

--- src/components/NavBar/NavBarElements.js
+++ src/components/NavBar/NavBarElements.js
@@ -20,7 +20,7 @@ export const Container = styled.div`
     padding: 0 2.2rem;
     display: flex;
     justify-content: space-between;
-    width:100%
+    width:100%;
 `
 
 export const NavMenue = styled.div`
@@ -29,6 +29,13 @@ export const NavMenue = styled.div`
 
     @media screen and (max-width: 780px){
         display: none;
+        display: ${({ isOpen }) => (isOpen ? 'flex' : 'none')};
+        flex-direction: column;
+        width: 93%;
+        background-color:rgb(29, 29, 31);
+        position: fixed;
+        left:0;
+        height: auto;
     }
 `
 
@@ -43,6 +50,11 @@ export const NavLogoLink = styled(Link)`
         opacity: .98;
         transition: 300ms ease-in;
     }
+
+    @media screen and (max-width: 780px){
+        display: none;
+        display: ${({ isOpen }) => (isOpen ? 'none' : 'flex')};
+    }
 `
 
 export const NavLink = styled(Link)`
@@ -50,6 +62,9 @@ export const NavLink = styled(Link)`
     display: flex;
     font-size: 1.2rem;
     align-items: center;
+    font-size: ${({ isOpen }) => isOpen ? '3rem' : '1.2rem'};
+    padding-top: ${({ isOpen }) => isOpen ? '10px' : '0px'};
+    padding-bottom: ${({ isOpen }) => isOpen ? '10px' : '0px'};
     text-decoration: none;
     padding-left: 2rem;
     padding-right: 1.5rem;
@@ -67,7 +82,7 @@ export const Bars = styled(FaBars)`
     display: none;
     color: #fff;
 
-    @media screen and (max-width: 760px){
+    @media screen and (max-width: 780px){
         display: block;
         position: absolute;
         top: 0;
@@ -75,6 +90,16 @@ export const Bars = styled(FaBars)`
         transform: translate(-100%, 75%);
         font-size: 1.8rem;
         cursor: pointer;
+    }
+`
+
+export const Container2 = styled.div`
+    display: flex;
+    padding-right:20px;
+
+    @media screen and (max-width: 780px){
+        display: none;
+        display: ${({ isOpen }) => (isOpen ? 'none' : 'flex')};
     }
 `
 
--- src/components/NavBar/NavIndex.js
+++ src/components/NavBar/NavIndex.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import AppleLogo from '../../images/icons/apple.svg';
 import SearchLogo from '../../images/icons/search.svg';
 import BagLogo from '../../images/icons/bag.svg';
-import { Nav, NavLink, Bars, NavMenue, NavLogoLink, Container} from './NavBarElements'
+import { Nav, NavLink, Bars, NavMenue, NavLogoLink, Container, Container2} from './NavBarElements'
 
 function NavBar(){
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const closeMenu = () => setIsOpen(false);
+
     return (
         <>
             <Nav>
@@ -16,44 +20,44 @@ function NavBar(){
                             alt="appleLogo"
                         />
                     </NavLogoLink>
-                    <Bars/>
-                    <NavMenue>
-                        <NavLink to = '/store' activeStyle>
+                    <Bars onClick={toggle}/>
+                    <NavMenue isOpen={isOpen}>
+                        <NavLink to = '/store' activeStyle isOpen={isOpen} onClick={closeMenu}>
                             Store
                         </NavLink>
-                        <NavLink to = '/mac' activeStyle>
+                        <NavLink to = '/mac' activeStyle isOpen={isOpen} onClick={closeMenu}>
                             Mac
                         </NavLink>
-                        <NavLink to = '/ipad' activeStyle>
+                        <NavLink to = '/ipad' activeStyle isOpen={isOpen} onClick={closeMenu}>
                             iPad
                         </NavLink>
-                        <NavLink to = '/iphone' activeStyle>
+                        <NavLink to = '/iphone' activeStyle isOpen={isOpen} onClick={closeMenu}>
                             iPhone
                         </NavLink>
-                        <NavLink to = '/watch' activeStyle>
+                        <NavLink to = '/watch' activeStyle isOpen={isOpen} onClick={closeMenu}>
                             Watch
                         </NavLink>
-                        <NavLink to = '/vision' activeStyle>
+                        <NavLink to = '/vision' activeStyle isOpen={isOpen} onClick={closeMenu}>
                             Vision
                         </NavLink>
-                        <NavLink to = '/airpods' activeStyle>
+                        <NavLink to = '/airpods' activeStyle isOpen={isOpen} onClick={closeMenu}>
                             AirPods
                         </NavLink>
-                        <NavLink to = '/tv&home' activeStyle>
+                        <NavLink to = '/tv&home' activeStyle isOpen={isOpen} onClick={closeMenu}>
                             TV & Home
                         </NavLink>
-                        <NavLink to = '/entertainment' activeStyle>
+                        <NavLink to = '/entertainment' activeStyle isOpen={isOpen} onClick={closeMenu}>
                             Entertainment
                         </NavLink>
-                        <NavLink to = '/accessories' activeStyle>
+                        <NavLink to = '/accessories' activeStyle isOpen={isOpen} onClick={closeMenu}>
                             Accessories
                         </NavLink>
-                        <NavLink to = '/support' activeStyle>
+                        <NavLink to = '/support' activeStyle isOpen={isOpen} onClick={closeMenu}>
                             Support
                         </NavLink>
                     </NavMenue>
-                    <div style={{display:'flex'}}>
-                        <NavLogoLink to = '/search'>
+                    <Container2 isOpen={isOpen}>
+                        <NavLogoLink to = '/search' >
                             <img
                                 src={SearchLogo}
                                 className="nav-logo"
@@ -65,10 +69,9 @@ function NavBar(){
                                 src={BagLogo}
                                 className="nav-logo"
                                 alt="bagLogo"
-                                style={{marginRight:"10px"}}
                             />
                         </NavLogoLink>
-                    </div>
+                    </Container2>
                 </Container>
             </Nav>
         </>
